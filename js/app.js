66f8e498d52961b571f41a73c435d0ad2d6e4b67
Hanan Mufti <hi5hanan@yahoo.com>
2017-06-25 23:46:39 +0200

    Ability to color on pad

--- js/app.js
+++ js/app.js
@@ -10,12 +10,14 @@ function SketchPad(num, color) {
 
 SketchPad.prototype.init = function() {
 
-    if (this.num) {
+    if (this.num && this.color) {
         this.num;
+        this.color;
     } else {
         this.num = 20;
+        this.color = '#000000';
     }
-    this.color = '#000000';
+
     this.render();
 
 }
@@ -27,30 +29,67 @@ SketchPad.prototype.render = function() {
     for (var i = 0; i < squared; i++) {
 
         if (i % this.num === 0) {
-            $('<div class="block" style="background-color:' + this.color + '"></div>').appendTo('#sketch');
+            $('<div class="block"></div>').appendTo('#sketch');
         } else {
-            $('<div class="block" style="background-color:' + this.color + '"></div>').appendTo('#sketch');
+            $('<div class="block"></div>').appendTo('#sketch');
         }
 
     }
 
-    jQuery('.block').css({ 'width': 600 / this.num, 'height': 600 / this.num });
+    jQuery('.block').css({ 'width': 600 / this.num, 'height': 600 / this.num, 'background-color': this.color });
+
 }
 
 SketchPad.prototype.reset = function() {
 
     $('#sketch').empty();
     this.num = 0;
-    while( this.num < 1 || this.num > 100 ) {
-        this.num = prompt('Select size between 1 and 100');
+
+    while( this.num < 1 || this.num > 60 ) {
+        this.num = prompt('Select size between 1 and 60');
     }
-    this.render();
+
+    this.init();
     
+}
+
+SketchPad.prototype.colorize = function() {
+    
+    var codes = '0123456789ABCDEF',
+        color = '#';
+
+    for(var c = 0; c < 6; c++) {
+        color += codes[Math.floor(Math.random() * 16)];
+    }
+
+    return color;
+
 }
 
 jQuery(document).ready(function($) {
 
-    $sketchpad = new SketchPad(16);
-    $sketchpad.init()
+    sketchpad = new SketchPad(10);
+    sketchpad.init();
+
+    $('#reset').click(function(e) {
+        e.preventDefault();
+
+        sketchpad.reset();
+        mouseMove();
+    });
+
+    function mouseMove() {
+
+        $('.block').mouseover(function() {
+            $(this).css('background-color', sketchpad.colorize());
+        });
+
+        $('.block').mouseleave(function() {
+            sketchpad.colorize();
+        });
+
+    }
+
+    mouseMove();
 
 });
